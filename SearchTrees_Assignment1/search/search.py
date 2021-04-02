3fcb849fa6f3591d9db77ba79672bc90522e478e
Breno Pejon <brepejon@gmail.com>
2021-04-02 15:24:57 +0900

    AStar Search Implemented

--- SearchTrees_Assignment1/search/search.py
+++ SearchTrees_Assignment1/search/search.py
@@ -87,12 +87,10 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
 
-    """Easy
+    """How it works
     First, we create a Stack.
-    Then, que will put the directions in order NSEW.
-    When the directions taken lead to an unavailable square, we put the next direction.
-    If all directions are unavala available, we head back and change the directions
-    We do this untill pacman reaches it goal.
+    Then we put a Node which saves the coordenate and path of Pacman
+    Moreover, we put on the Stack all of the 
     """
 
     #2 types of Implementation
@@ -126,6 +124,8 @@ def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
     #Same thing as the DFS above, although it's implemented with a Queue
+    """So instead of exploring in deep, it explores in breadth"""
+
     Queue = util.Queue()
     Queue.push((problem.getStartState(), []))
 
@@ -150,7 +150,11 @@ def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
     #Easy, the same thing as DFS and BFS although with costs
-
+    """
+    To create a UniformCostSeach, we will need a Priority Queue
+    The Queue will store One Node with the Coordinates, the Path and the Total Path Cost,
+    and the priority will be the cost of the actual node 
+    """
     PrioQueue = util.PriorityQueue()
     PrioQueue.push((problem.getStartState(), [], 0), 0)
 
@@ -166,8 +170,8 @@ def uniformCostSearch(problem):
             VisitedNodes.append(CurrentNode)
             for NextNode, Action, ActionCost in problem.getSuccessors(CurrentNode):
                 NewPath = Path + [Action]
-                TotalCost = PathCost + ActionCost
-                PrioQueue.push((NextNode, NewPath, ActionCost), TotalCost)
+                NewPathCost = PathCost + ActionCost
+                PrioQueue.push((NextNode, NewPath, ActionCost), NewPathCost)
 
 
     else:
@@ -184,8 +188,31 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    #The same structure as UCS but the priority now is the heuristics
+    PrioQueue = util.PriorityQueue()
+    PrioQueue.push((problem.getStartState(), [], 0), 0)
+
+    VisitedNodes = []
+    Path = []
+
+    while PrioQueue.isEmpty() != True:
+        CurrentNode, Path, PathCost = PrioQueue.pop()
+        if problem.isGoalState(CurrentNode) == True:
+            return Path
+        
+        if CurrentNode not in VisitedNodes:
+            VisitedNodes.append(CurrentNode)
+            for NextNode, Action, ActionCost in problem.getSuccessors(CurrentNode):
+                NewPath = Path + [Action]
+                NewPathCost = PathCost + ActionCost
+                NewPathCost += heuristic(NextNode, problem) 
+                PrioQueue.push((NextNode, NewPath, ActionCost), NewPathCost)
+
+
+    else:
+        print "Path is Blocked!"
+        return -1
+
 
 
 # Abbreviations
